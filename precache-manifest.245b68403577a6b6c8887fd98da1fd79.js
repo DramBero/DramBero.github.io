c0c257f4aaefa529aa1df0702b037769001ad8a3
DramBero <60761907+DramBero@users.noreply.github.com>
2023-03-07 01:17:04 +0300

    Add files via upload

---
+++ precache-manifest.245b68403577a6b6c8887fd98da1fd79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6abcb94503c996f12d7",
+    "url": "/css/app.0be08d2b.css"
+  },
+  {
+    "revision": "e6abcb94503c996f12d7",
+    "url": "/js/app.3a5fe309.js"
+  },
+  {
+    "revision": "20ada51f06f4cfd8585a",
+    "url": "/js/chunk-vendors.eddaa6b7.js"
+  },
+  {
+    "revision": "ee7729051eb2faa01d864c4eb0c282db",
+    "url": "/fonts/Pelagiad.ee772905.ttf"
+  },
+  {
+    "revision": "573cc726959e0e48256b4db7a0761491",
+    "url": "/index.html"
+  }
+];
