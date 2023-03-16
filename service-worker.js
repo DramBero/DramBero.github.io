6ffe10e0c0f025b26a3c35eb7f0c803214ee1574
DramBero <60761907+DramBero@users.noreply.github.com>
2023-03-16 03:09:02 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8434dd5e72fbe130dddcea130dd0168b.js"
+  "/precache-manifest.71e08fda04edacb56c84e8467ffc7dc9.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "teoqed"});
